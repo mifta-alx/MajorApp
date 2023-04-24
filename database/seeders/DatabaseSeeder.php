5413f0f6d9ba15f3eb19ce2e9cefeac92b84df28
alx-cpu <alx.yoi67@gmail.com>
2023-04-24 10:42:50 +0700

    Add Criteria Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Criteria;
+use App\Models\Alternative;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Criteria::create([
+            'criteria_name' => 'nilai_uts',
+            'criteria_label' => 'Nilai UTS',
+            'weight' => '0.15'
+        ]);
+        Criteria::create([
+            'criteria_name' => 'nilai_uas',
+            'criteria_label' => 'Nilai UAS',
+            'weight' => '0.15'
+        ]);
+        Criteria::create([
+            'criteria_name' => 'nilai_un',
+            'criteria_label' => 'Nilai UN',
+            'weight' => '0.30'
+        ]);
+        Criteria::create([
+            'criteria_name' => 'rata_rapor',
+            'criteria_label' => 'Rata Rata Rapor',
+            'weight' => '0.25'
+        ]);
+        Criteria::create([
+            'criteria_name' => 'minat',
+            'criteria_label' => 'Minat',
+            'weight' => '0.15'
+        ]);
     }
 }
